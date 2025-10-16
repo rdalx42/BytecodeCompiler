5dd744dadd8dc1828cd56c66152b392795c322ad
rd <hopartean.radu@spectrum.ro>
2025-10-16 17:06:29 +0300

    more tokens

--- bytelang/src/headers/backend/lexer.h
+++ bytelang/src/headers/backend/lexer.h
@@ -2,6 +2,7 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+#include <unordered_map>
 #include <vector>
 #include <string>
 
@@ -12,6 +13,7 @@ extern const std::vector<std::string> keywords;
 extern const std::vector<std::string> bytecode_keywords;
 
 enum TOKEN_T{
+
     INT,
     FLOAT,
     OP,
@@ -20,13 +22,21 @@ enum TOKEN_T{
     KEYWORD,
     IDENTIFIER,
     NEWLINE,
-    
-    // bytecode
+    GOTO_LABEL,
 
+    // bytecode
+    BYTECODE_GOTO_LABEL,
+    BYTECODE_GOTO,
     BYTECODE_NEG,
     BYTECODE_PUSH,
     BYTECODE_POP,
     BYTECODE_ADD,
+    BYTECODE_GT,
+    BYTECODE_LT,
+    BYTECODE_LTE,
+    BYTECODE_GTE,
+    BYTECODE_EQ,
+    BYTECODE_NOTEQ,
     BYTECODE_SUB,
     BYTECODE_TOP,
     BYTECODE_MUL,
@@ -34,7 +44,11 @@ enum TOKEN_T{
     BYTECODE_STORE,
     BYTECODE_LOAD,
     BYTECODE_POP_ALL,
-    BYTECODE_CLEANUP
+    BYTECODE_CLEANUP,
+    BYTECODE_MAKE_BLOCK,
+    BYTECODE_DEL_BLOCK,
+    BYTECODE_SAFETY,
+    BYTECODE_GOTO_IF_ZERO,
 };
 
 struct TOKEN{
@@ -47,10 +61,13 @@ struct LEXER{
     std::string content;
     size_t pos = 0;
     std::vector<TOKEN> tokens;
+    
+    std::unordered_map<std::string,int>goto_positions;
+
     LEXER(const std::string& c) : content(c), pos(0) {}
 };
 
-char peek(LEXER& lex);
+char peek(LEXER& lex,int&index);
 
 void doidentifier(LEXER& lexer, int& index);
 void donum(LEXER& lexer, int& index);
