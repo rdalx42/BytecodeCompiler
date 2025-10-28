d33934607e0d87817c3b256212f543b860318056
rd <hopartean.radu@spectrum.ro>
2025-10-28 18:01:23 +0200

    print bytecode

--- bytelang/src/compiler/compiler.h
+++ bytelang/src/compiler/compiler.h
@@ -14,6 +14,8 @@
 struct COMPILER {
     std::string bytecode;
     MEMORY memory;
+    CALL_STACK function_call_stack;
+   // UTILS& ut;
     AST& ast;              
     LEXER& lex;           
     
@@ -25,6 +27,7 @@ COMPILER init(AST& ast, LEXER& lex);
 void generate_bytecode(COMPILER& comp, AST_NODE* nd);
 void compile_ast_to_bytecode(COMPILER& comp);
 void compile(COMPILER& comp);
+void print_bytecode(COMPILER& comp) ;
 
 
 #endif
