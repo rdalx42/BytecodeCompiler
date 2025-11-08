40177d19c8eb902c58f490167e4883e324b7f02a
rd <hopartean.radu@spectrum.ro>
2025-11-08 14:15:36 +0200

    pre init data

--- bytelang/src/compiler/compiler.h
+++ bytelang/src/compiler/compiler.h
@@ -14,16 +14,20 @@
 struct COMPILER {
     std::string bytecode;
     MEMORY memory;
+    
     CALL_STACK function_call_stack;
    // UTILS& ut;
     AST& ast;              
     LEXER& lex;           
+
+    std::vector<FAST_STRING_COMPONENT>preloaded_strings_per_token;
     
     COMPILER(AST& a, LEXER& l) : ast(a), lex(l){}
 };
 
 COMPILER init(AST& ast, LEXER& lex);
 
+void pre_init_data(COMPILER& comp);
 void generate_bytecode(COMPILER& comp, AST_NODE* nd);
 void compile_ast_to_bytecode(COMPILER& comp);
 void compile(COMPILER& comp);
