71b193dafd5763eaa73c4ff25c67a9ca2d38058a
rd <hopartean.radu@spectrum.ro>
2025-11-13 19:32:45 +0200

    display_non_fatal_err

--- bytelang/src/runtime/err.cpp
+++ bytelang/src/runtime/err.cpp
@@ -10,3 +10,10 @@ void display_err(const std::string& message) {
 
     std::exit(EXIT_FAILURE);
 }
+
+void display_non_fatal_err(const std::string& message){
+    const std::string RED   = "\033[31m";  
+    const std::string RESET = "\033[0m";   
+
+    std::cerr << RED << "[Error]: " << message << RESET << std::endl;
+}
